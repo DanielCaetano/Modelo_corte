a48af620f339e377c4d69e51166a4cc02e29a38e
DanielCaetano <37419231+DanielCaetano@users.noreply.github.com>
2019-09-24 00:04:07 -0300

    Update modeloJL.jl

--- modeloJL.jl
+++ modeloJL.jl
@@ -2,13 +2,15 @@ using JuMP
 using Cbc
 
 #modelo lendo arquo ntancas
+lista = ["rinstance_01_2pol.txt","rinstance_01_3pol.txt","sinstance_01_2pol_sep.txt","sinstance_01_3pol_sep.txt"]
 
-file = ["sinstance_01_1pol_sep.txt","sinstance_01_2pol_sep.txt","sinstance_01_3pol_sep.txt","sinstance_01_4pol_sep.txt","sinstance_01_5pol_sep.txt","sinstance_01_6pol_sep.txt","sinstance_01_7pol_sep.txt","sinstance_01_8pol_sep.txt","sinstance_01_9pol_sep.txt","sinstance_01_10pol_sep.txt"]
+lista1 = ["rinstance_01_4pol.txt","rinstance_01_5pol.txt","sinstance_01_5pol_sep.txt"]
+
+lista2 = ["sinstance_01_2pol_sep.txt","sinstance_01_3pol_sep.txt","sinstance_01_4pol_sep.txt"]
 
 function modelo_final(file::Array)
     while file != []
         ele = file[1]
-        println(ele)
         entrada = readlines("/home/daniel/Documentos/projetos/julia/resolucao/entradas/$ele")
         vertex = Dict{Int, Any}() # vertices
         edges= Dict{Int, Any}() # arestas
@@ -21,7 +23,7 @@ function modelo_final(file::Array)
         popfirst!(entrada)
         function criar_vertex()
             ps = 1
-            println("PS: ",ps)
+            #println("PS: ",ps)
             for i in entrada
                 linha = split(i,' ')
                 println(linha)
@@ -30,13 +32,10 @@ function modelo_final(file::Array)
                 t1=true
                 t2=true
                 for k in 1:length(vertex)
-                    #println("p1: $p1 p2: $p2 k:",vertex[k],"\n")
                     if p1 == vertex[k]
-                        #println("Repetiu p1:  --------- $p1")
                         t1=false
                     end
                     if p2 == vertex[k]
-                        #println("Repetiu p2:  --------- $p2")
                         t2=false
                     end
                 end
@@ -55,7 +54,6 @@ function modelo_final(file::Array)
                         end
                     end
                 end
-                #println(ponto)
             end
 
         end
@@ -73,20 +71,16 @@ function modelo_final(file::Array)
                 ky1=0.0
                 ky2=0.0
                 while j <= length(vertex)
-                    #println("p: $p1 I: $i")
                     if p1 == vertex[j]
-                        #println("p1 -------- $j")
                         t1=true
                         ky1=j
                     end
                     if p2 == vertex[j]
-                        #println("p2 ------- $j")
                         t2=true
                         ky2=j
                     end
 
                     if t1 == true && t2 == true
-                        #println("$ky1 ------- $ky2")
                         edges[ps]=(ky1, ky2)
                         ps+=1
                         break
@@ -105,17 +99,13 @@ function modelo_final(file::Array)
         #pop!(edges)
 
         #Inicio para modelo
-        println("MATRIZ ",length(vertex))
+        println("MATRIZ ")
         t_cut=1
         t_deslocate =5
         nVertex = length(vertex)
-        nedges = length(edges)
+        nEdges = length(edges)
         # Matriz para amazenar as distâncias entre os vertex
-
-
         dist = zeros(nVertex, nVertex)
-
-        # Definição da posição de cada vértice para o desenho
         posX, posY = [], []
         for i in sort(collect(keys(vertex)))
             posI = vertex[i]
@@ -127,10 +117,11 @@ function modelo_final(file::Array)
             append!(posY, posI[2])
         end
 
-
-        #ADJACENTES
+        #Arestas de corte
+        #Eges of cut
         Ac = zeros(nVertex, nVertex)
         # Definição da posição de cada vertice para o desenho
+        # Setting the position of each vertex for the drawing
 
         for i in sort(collect(keys(edges)))
             Ac[edges[i][1], edges[i][2]] = dist[edges[i][1], edges[i][2]]
@@ -139,12 +130,18 @@ function modelo_final(file::Array)
 
 
         mis = zeros(nVertex, nVertex)
-
         # Definição da posição de cada vértice para o desenho
         for i in sort(collect(keys(vertex)))
             for j in sort(collect(keys(vertex)))
                 mis[i,j]= dist[i,j]/t_deslocate
             end
+        end
+
+        pis = zeros(nEdges, nEdges)
+        # Definição da posição de cada vértice para o desenho
+        for i in sort(collect(keys(edges)))
+                pis[edges[i][1], edges[i][2]] = dist[edges[i][1], edges[i][2]]/t_cut
+                #println(pis[edges[i][1], edges[i][2]])
         end
 
         #solver Cbc para modelo matematico
@@ -164,28 +161,28 @@ function modelo_final(file::Array)
         t_inicio = time()
         # Definição da variável matricial Xijy e Yt
         # Note que a variável nVertex foi definida anteriormente após dicionario com as cidades
-        @variable(model,X[i=1:nVertex,j=1:nVertex, t=1:2*nedges; i!=j],Bin)
-        @variable(model,Y[j=1:nVertex,t=1:2*nedges],Bin)
+        @variable(model,X[i=1:nVertex,j=1:nVertex, t=1:2*nEdges; i!=j],Bin)
+        @variable(model,Y[j=1:nVertex,t=1:2*nEdges],Bin)
 
         # Função objetivo
-        @objective(model, Min, sum(X[i, j, t] * dist[i, j]/t_deslocate for i in 1:nVertex, j in 1:nVertex, t in 1:2*nedges if i != j && (Ac[i,j]==0 || Ac[j,i]==0))
-                +sum((((sum((X[edges[i][1], edges[i][2], t]+ X[edges[i][2], edges[i][1], t]) for t in 1:2*nedges)-1)
-                    *dist[edges[i][1], edges[i][2]])/t_deslocate)+dist[edges[i][1], edges[i][2]]/t_cut for i in 1:length(edges))
+        @objective(model, Min, sum(X[i, j, t] * mis[i, j] for i in 1:nVertex for j in 1:nVertex for t in 1:2*nEdges if i != j && (Ac[i,j]==0 && Ac[j,i]==0))
+                +sum(((sum((X[edges[i][1], edges[i][2], t]+ X[edges[i][2], edges[i][1], t]) for t in 1:2*nEdges)-1)
+                    *mis[edges[i][1], edges[i][2]])+pis[edges[i][1], edges[i][2]] for i in 1:nEdges)
                 +sum(X[i,j,1] * (((vertex[i][1]-0)^2+(vertex[i][2]-0)^2 )^(1/2)) for i in 1:nVertex for j in 1:nVertex if i != j))
 
         #(1)
-        for t in 1:2*nedges
+        for t in 1:2*nEdges
             @constraint(model, sum(X[i,j,t] for i in 1 : nVertex for j in 1 : nVertex  if i != j ) <= 1)
         end
 
         #(2)
-        for i in 1:nedges
+        for i in 1:nEdges
             @constraint(model,sum(X[edges[i][1],edges[i][2],t] + X[edges[i][2],edges[i][1],t]
-             for t in 1:2*nedges) >= 1)
+             for t in 1:2*nEdges) >= 1)
         end
         #(3)
         for j in 1:nVertex
-            for t in 2:(2*nedges)
+            for t in 2:(2*nEdges)
                 @constraint(model,((sum(X[i,j,t-1] for i in 1:nVertex if i!=j)
                 -sum(X[j,k,t] for k in 1:nVertex if k!=j))-Y[j, t]) == 0)
             end
@@ -201,14 +198,15 @@ function modelo_final(file::Array)
 
         arq1 = open("/home/daniel/Documentos/projetos/julia/resolucao/resultados/res_$ele", "a")
         write(arq1, "PROBLEM: $ele\n")
-        for t in 1:2*nedges
+        for t in 1:2*nEdges
             for i in 1:nVertex
                 for j in 1:nVertex
                     if i != j && JuMP.value(X[i, j,t])!=0
                         d=dist[i,j]
                         soma+=d
-                        println("Aresta:( $i, $j ), Distance: $d  T: $t ")
-                        write(arq1, "A:($i, $j) Distance: $d  T: $t \n")
+                        v_mis = mis[i,j]
+                        println("Aresta:( $i, $j ), Distance: $d  T: $t mis: $v_mis")
+                        write(arq1, "A:($i, $j) Distance: $d  T: $t mis: $v_mis\n")
                     end
                 end
             end
@@ -227,8 +225,53 @@ function modelo_final(file::Array)
         println("Tempo total sistema $timeFinal maior que tempo do modelo JuMP: ")
         println("Distancia total: $soma")
 
+        fo1 = 0
+        fo2 = 0
+        fo3 = 0
+        for i in 1:nVertex
+            for j in 1:nVertex
+                for t in 1:2*nEdges
+                    if i != j && (Ac[i,j]==0 && Ac[j,i]==0)
+                        if JuMP.value(X[i, j, t] ) != 0.0
+                            println("X($i, $j, $t)")
+                        end
+                        fo1 = fo1 + (JuMP.value(X[i, j, t])* mis[i, j])
+                    end
+                end
+            end
+        end
+
+        fo2_s = 0
+        for i in 1:nEdges
+            fo2_s=0
+            for t in 1:2*nEdges
+                fo2_s =fo2_s+ JuMP.value(X[edges[i][1], edges[i][2], t])+ JuMP.value(X[edges[i][2], edges[i][1], t])
+            end
+            a1=edges[i][1]
+            a2=edges[i][2]
+
+            fo2 =fo2 + (((fo2_s-1)*mis[edges[i][1], edges[i][2]] )+ pis[edges[i][1], edges[i][2]])
+            pis_t = pis[edges[i][1], edges[i][2]]
+            mis_t = mis[edges[i][1], edges[i][2]]
+            println("Corte ($a1, $a2) t: $fo2_s FO2: $fo2 , pis: $pis_t mis: $mis_t")
+        end
+
+        for i in 1:nVertex
+            for j in 1:nVertex
+                if i != j
+                    fo3 = fo3 + (JuMP.value(X[i,j,1]) * ((vertex[i][1]-0)^2+(vertex[i][2]-0)^2 ))^(1/2)
+                end
+            end
+        end
+        #fo1 = sum(X[i, j, t] * mis[i, j] for i in 1:nVertex for j in 1:nVertex, t in 1:2*nEdges if i != j && (Ac[i,j]==0 || Ac[j,i]==0))
+        #fo2 = sum(((sum( for t in 1:2*nEdges)-1)
+        #            *mis[edges[i][1], edges[i][2]])+pis[edges[i][1], edges[i][2]] for i in 1:nEdges)
+        #fo3 = sum(X[i,j,1] * (((vertex[i][1]-0)^2+(vertex[i][2]-0)^2 )^(1/2)) for i in 1:nVertex for j in 1:nVertex if i != j)
+
+        println("F1: $fo1 F2: $fo2 F3: $fo3")
+
         popfirst!(file)
     end
 end
 
-modelo_final(file)
+modelo_final(lista1)
